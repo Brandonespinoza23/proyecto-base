185ad4cd47d790eca28b0f534c91895b65412036
Brandon Espinoza <64062leo@universidaddeleon.edu.mx>
2025-09-24 18:01:54 -0600

    Agregué operaciones faltantes

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -1,29 +1,47 @@
+
 class Operaciones:
     def __init__(self):
         self.num1 = 0
         self.num2 = 0
         self.resultado = 0
-        
+    
     def leerNumeros(self):
         while True:
             try:
-                self.num1 = int(input("Número 1:"))
+                self.num1 = int(input("Número 1: "))
                 break
             except Exception:
                 print("Número inválido")
                 continue
         while True:
             try:
-                self.num2 = int(input("Número 2:"))
+                self.num2 = int(input("Número 2: "))
                 break
             except Exception:
                 print("Número inválido")
                 continue    
     
     def sumar(self):
-        self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igula a " + str(self.num1 + self.num2)
+        self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igual a " + str(self.num1 + self.num2)
+    
+    def restar(self):
+        self.resultado = "La resta de " + str(self.num1) + " - " + str(self.num2) + " es igual a " + str(self.num1 - self.num2)
+    
+    def multiplicar(self):
+        self.resultado = "La multiplicación de " + str(self.num1) + " * " + str(self.num2) + " es igual a " + str(self.num1 * self.num2)
+    
+    def dividir(self):
+        if self.num2 == 0:
+            self.resultado = "Error: No se puede dividir entre 0"
+        else:
+            self.resultado = "La división de " + str(self.num1) + " / " + str(self.num2) + " es igual a " + str(self.num1 / self.num2)
+    
+    def modulo(self):
+        if self.num2 == 0:
+            self.resultado = "Error: No se puede calcular el módulo con divisor 0"
+        else:
+            self.resultado = "El módulo de " + str(self.num1) + " % " + str(self.num2) + " es igual a " + str(self.num1 % self.num2)
     
     def mostrarResultado(self):
         print(self.resultado)
-        
-        
+
